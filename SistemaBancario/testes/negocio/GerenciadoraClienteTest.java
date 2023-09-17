45715a7bd3d1b57618fd1b194b3e0e5e262fda6d
LucasMatulis <lucas.matulis@gmail.com>
2023-09-17 16:39:04 -0300

    das

--- SistemaBancario/testes/negocio/GerenciadoraClienteTest.java
+++ SistemaBancario/testes/negocio/GerenciadoraClienteTest.java
@@ -19,13 +19,13 @@ import negocio.GerenciadoraClientes;
 public class GerenciadoraClienteTest {
 
     private GerenciadoraClientes gerClientes;
-    private int idCLiente01=1;
-    private int idCLiente02=2;
+    private int idCliente01=1;
+    private int idCliente02=2;
 
     @Before
     public void setUp(){
-        Cliente cliente01= new Cliente(idCLiente01, "João", 31, "joao@gmail.com",1,true);
-        Cliente cliente02= new Cliente(idCLiente02, "Maria", 34, "maria@gmail.com",1,true);
+        Cliente cliente01= new Cliente(idCliente01, "João", 31, "joao@gmail.com",1,true);
+        Cliente cliente02= new Cliente(idCliente02, "Maria", 34, "maria@gmail.com",1,true);
 
         List<Cliente> clientesDoBanco= new ArrayList<>();
         clientesDoBanco.add(cliente01);
@@ -42,19 +42,45 @@ public class GerenciadoraClienteTest {
     @Test
     public void testPesquisaCliente(){
 
-        Cliente cliente= gerClientes.pesquisaCliente(idCLiente01);
+        Cliente cliente= gerClientes.pesquisaCliente(idCliente01);
 
-        assertThat(cliente.getId(), is(idCLiente01));
+        assertThat(cliente.getId(), is(idCliente01));
     }
 
     @Test
     public void testRemoveCliente(){
 
-        boolean clienteRemovido= gerClientes.removeCliente(idCLiente02);
+        boolean clienteRemovido= gerClientes.removeCliente(idCliente02);
 
         assertThat(clienteRemovido,is(true));
         assertThat(gerClientes.getClientesDoBanco().size(), is(1));
-        assertNull(gerClientes.pesquisaCliente(idCLiente02));
+        assertNull(gerClientes.pesquisaCliente(idCliente02));
     }
+    
+    
+    //Validar a idade do cliente
+    @Test
+	public void testValidarIdade() {
+		try {
+			assertThat(gerClientes.validaIdade(gerClientes.pesquisaCliente(idCliente01).getIdade()), is(true));
+		} catch (IdadeNaoPermitidaException e) {
+			e.printStackTrace();
+		}
+		try {
+			assertThat(gerClientes.validaIdade(gerClientes.pesquisaCliente(idCliente02).getIdade()), is(true));
+		} catch (IdadeNaoPermitidaException e) {
+			e.printStackTrace();
+		}
+		try {
+			assertThat(gerClientes.validaIdade(17), is(true));
+		} catch (IdadeNaoPermitidaException e) {
+			e.printStackTrace();
+		}
+		try {
+			assertThat(gerClientes.validaIdade(66), is(true));
+		} catch (IdadeNaoPermitidaException e) {
+			e.printStackTrace();
+		}
+	}
     
 }
--- SistemaBancario/testes/negocio/GerenciadoraContasTest.java
+++ SistemaBancario/testes/negocio/GerenciadoraContasTest.java
@@ -12,13 +12,16 @@ public class GerenciadoraContasTest {
 	
 	private GerenciadoraContas gerContas;
 	
+	int idConta01=1;
+	int idConta02=2;
 	
+	
+	
+	//Saldo suficiente em ambas as contas
 	@Test
 	public void testTransfereValor() {
-		int idConta01=1;
-		int idConta02=2;
-		ContaCorrente conta01= new ContaCorrente(idConta01,100,true);
-		ContaCorrente conta02= new ContaCorrente(idConta02,100,true);
+		ContaCorrente conta01= new ContaCorrente(idConta01,200,true);
+		ContaCorrente conta02= new ContaCorrente(idConta02,0,true);
 		
 		List<ContaCorrente> contasDoBanco= new ArrayList<>();
 		contasDoBanco.add(conta01);
@@ -27,12 +30,69 @@ public class GerenciadoraContasTest {
 		gerContas= new GerenciadoraContas(contasDoBanco);
 		
 		
-		boolean sucesso= gerContas.transfereValor(idConta02, 100, idConta02);
+		boolean sucesso= gerContas.transfereValor(idConta01, 100, idConta02);
 		
 		assertTrue(sucesso);
 		assertThat(conta02.getSaldo(), is(100.0));
 		assertThat(conta01.getSaldo(), is(100.0));
 
+	}
+	
+	
+	//Saldo insuficiente e positivo na Conta 1
+	@Test
+	public void testTransfereValor02() {
+		ContaCorrente conta01 = new ContaCorrente(idConta01, 100, true);
+		ContaCorrente conta02 = new ContaCorrente(idConta02, 0, true);
+		
+		List<ContaCorrente> contasDoBanco = new ArrayList<>();
+		contasDoBanco.add(conta01);
+		contasDoBanco.add(conta02);
+		
+		GerenciadoraContas gerContas = new GerenciadoraContas(contasDoBanco);
+		boolean sucesso = gerContas.transfereValor(idConta01, 200, idConta02);
+		
+		assertTrue(sucesso);
+		assertThat(conta01.getSaldo(), is(-100.0));
+		assertThat(conta02.getSaldo(), is(200.0));
+	}
+	
+	
+	//Saldo insuficiente e negativo na Conta 1
+	@Test
+	public void testTransfereValor03() {
+		ContaCorrente conta01 = new ContaCorrente(idConta01, -100, true);
+		ContaCorrente conta02 = new ContaCorrente(idConta02, 0, true);
+		
+		List<ContaCorrente> contasDoBanco = new ArrayList<>();
+		contasDoBanco.add(conta01);
+		contasDoBanco.add(conta02);
+		
+		GerenciadoraContas gerContas = new GerenciadoraContas(contasDoBanco);
+		boolean sucesso = gerContas.transfereValor(idConta01, 200, idConta02);
+		
+		assertTrue(sucesso);
+		assertThat(conta01.getSaldo(), is(-300.0));
+		assertThat(conta02.getSaldo(), is(200.0));
+	}
+	
+	
+	//Saldoo insuficiente em ambas as contas
+	@Test
+	public void testTransfereValor04() {
+		ContaCorrente conta01 = new ContaCorrente(idConta01, -100, true);
+		ContaCorrente conta02 = new ContaCorrente(idConta02, -100, true);
+		
+		List<ContaCorrente> contasDoBanco = new ArrayList<>();
+		contasDoBanco.add(conta01);
+		contasDoBanco.add(conta02);
+		
+		GerenciadoraContas gerContas = new GerenciadoraContas(contasDoBanco);
+		boolean sucesso = gerContas.transfereValor(idConta01, 200, idConta02);
+		
+		assertTrue(sucesso);
+		assertThat(conta01.getSaldo(), is(-300.0));
+		assertThat(conta02.getSaldo(), is(100.0));
 	}
 
 }
